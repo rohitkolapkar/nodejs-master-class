18aff5380e2a1fe902a338d7518e73c4fec1ceb8
rohitkolapkar <rohitkolapkar333@gmail.com>
2019-03-13 11:24:58 +0530

    added global error handling and logging for 1. express req pipeline 2. In-App errors (sync & async ) using winston and project restructured

--- 10-vidly-project-with-authorization-authentication/middleware/error.js
+++ 10-vidly-project-with-authorization-authentication/middleware/error.js
@@ -1,3 +1,7 @@
+// this error.js is only for express - request processing pipeline 
+
+//any error in the app itself will not be handled by this. 
+
 const winston=require('winston');
 
 module.exports=function(err,req,res,next){
